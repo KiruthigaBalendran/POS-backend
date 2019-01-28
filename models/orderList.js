228a357d78d94c47331ea2ae35e215248fd59466
Kiruthiga Balendran <kiruthiga03@gmail.com>
2019-01-28 09:12:42 +0530

    Update orderList.js

--- models/orderList.js
+++ models/orderList.js
@@ -3,10 +3,10 @@ var db = require('../dbConnection');
 
 var order = {
     getAllOrderLists: function (callback) {
-        return db.query("select orderId, customerName,created from orderList where status=?", ["open"], callback);
+        return db.query("select orderId, customerName,created from orderlist where status=?", ["open"], callback);
     },
     getOrderListById: function (id, callback) {
-        return db.query("select orderId,customerName,created from orderList where orderId=? and status=?", [id,"open"], callback);
+        return db.query("select orderId,customerName,created from orderlist where orderId=? and status=?", [id,"open"], callback);
     },
     getMenuListById: function (id, callback) {
         return db.query("select * from menu where orderId=?", [id], callback);
@@ -15,28 +15,28 @@ var order = {
         return db.query("select * from menu where orderId=? and itemId=?", [orderId,itemId], callback);
     },
     addOrderList: function (order,callback) {
-        return db.query("Insert into orderList values(?,?,?,?)", [order.orderId, order.customerName, order.created,"open"], callback);
+        return db.query("Insert into orderlist values(?,?,?,?)", [order.orderId, order.customerName, order.created,"open"], callback);
     },
     addMenuById: function (id,menu,callback) {
         return db.query("Insert into menu values(?,?,?,?,?)", [id,menu.itemId,menu.itemName,menu.unitPrice,menu.quantity], callback);
     },
     deleteOrderList: function (id, callback) {
-        return db.query("delete from orderList where orderId=?", [id], callback);
+        return db.query("delete from orderlist where orderId=?", [id], callback);
     },
     deleteMenuById:  function (orderId,itemId, callback) {
         return db.query("delete from menu where orderId=? and itemId=?", [orderId,itemId], callback);
     },
     updateOrderList: function (id, order, callback) {
-        return db.query("update orderList set customerName=?,created=?,status=? where orderId=?", [order.customerName, order.created,"open", id], callback);
+        return db.query("update orderlist set customerName=?,created=?,status=? where orderId=?", [order.customerName, order.created,"open", id], callback);
     },
     updateMenuById: function (orderId,itemId, menu, callback) {
         return db.query("update menu set itemId=?,itemName=?,unitPrice=?,quantity=? where orderId=? and itemId=?", [menu.itemId,menu.itemName,menu.unitPrice,menu.quantity, orderId,itemId], callback);
     },
     getMaxOrderId: function (callback) {
-        return db.query("select max(orderId) from orderList;", callback);
+        return db.query("select max(orderId) from orderlist;", callback);
     },
     totalOrderList: function (callback) {
-        return db.query("select count(*) from orderList;", callback);
+        return db.query("select count(*) from orderlist;", callback);
     }
 };
 module.exports = order;
